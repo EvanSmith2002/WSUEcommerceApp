1a7d091c4b259d73c4ba1da0f041ba4ce404458e
NicholasZ42 <nicholas.zheng@wsu.edu>
2024-04-07 21:41:31 -0600

    Added stripe payment functionality

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 // sk_test_51LnUKJDM1jwCEz8OJG69szv032rIo4X0WrFMaXrqxu9g8fdohsL1y54JEUhFUKrqoBquVjN3AzpIFyrbN915bgcd00O5hqoGCJ
+const SECRET_KEY = 'sk_test_51P38GFP79apvj9gn7ok8QcBblRTZkJxeBmRtrCbFdO7VVok5QpQtAdPZ9DvG4PnytfqO8NXbZBBoaRTj3Id62Yo000iUYIKU6B'
 // Coffee: price_1LnUTFDM1jwCEz8OGoOSXiSM
 // Sunglasses: price_1LnUTxDM1jwCEz8OAqHYTwKQ
 // Camera: price_1LnUUoDM1jwCEz8OvxIcJ7to
 const express = require('express');
 var cors = require('cors');
-const stripe = require('stripe')('INSERT SECRET HERE');
+const stripe = require('stripe')(SECRET_KEY);
 
 const app = express();
 app.use(cors());
